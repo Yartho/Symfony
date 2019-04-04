f46f67a59a4e6481e0cf74852f48ece95f3ed9fc
yannhoffmann <yann.hoffmann@icloud.com>
2019-04-04 10:15:22 +0200

    register start

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Controller;
+
+
+class RegistrationController
+
+    /**
+     * @Route("/register", name="register")
+     */
+{
+    public function registerAction()
+    {
+        //Create form
+
+        //Check if the form is valid
+            // Then
+            // Hash the password
+            // Register the new user in the database
+
+        // render the register.html.twig
+    }
+
+}
